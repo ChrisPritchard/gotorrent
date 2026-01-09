21b6f2e14ed485c91afb384cb200c7cb8ac6fdea
Chris <chris@grislygrotto.nz>
2026-01-10 09:47:00 +1300

    code to parse a message

--- internal/peer/peer_messaging.go
+++ internal/peer/peer_messaging.go
@@ -1,7 +1,10 @@
 package peer
 
 import (
+	"bufio"
 	"encoding/binary"
+	"fmt"
+	"io"
 	"net"
 )
 
@@ -25,7 +28,7 @@ func keep_alive(conn net.Conn) error {
 	return err
 }
 
-func message(conn net.Conn, kind peer_message_type, data []byte) error {
+func send_message(conn net.Conn, kind peer_message_type, data []byte) error {
 	length := len(data) + 1           // 1 for the message type
 	to_send := make([]byte, 4+length) // first four bytes are where we put the length
 	binary.BigEndian.PutUint32(to_send, uint32(length))
@@ -35,4 +38,27 @@ func message(conn net.Conn, kind peer_message_type, data []byte) error {
 
 	_, err := conn.Write(to_send)
 	return err
+}
+
+func receive_message(conn net.Conn) (peer_message_type, []byte, error) {
+	reader := bufio.NewReader(conn)
+
+	len_bytes, err := reader.Peek(4)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	length := binary.BigEndian.Uint32(len_bytes)
+	received := make([]byte, 4+length) // we include the 4 bytes we peeked
+	n, err := io.ReadFull(reader, received)
+	if err != nil {
+		return 0, nil, err
+	}
+	if n != int(length)+4 {
+		return 0, nil, fmt.Errorf("was expecting %d bytes, but only received %d", length+4, n)
+	}
+
+	kind := peer_message_type(received[4])
+	data := received[5:]
+	return kind, data, nil
 }
